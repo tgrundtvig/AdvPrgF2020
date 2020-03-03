ce6a4cc1357f637b14c08c0004771316f64a194a
Tobias <tgrundtvig@gmail.com>
2020-03-03 10:17:41 +0100

    Moved and renamed project

--- Projects/ThreadsAndGenerics/src/blocks/basic/examples/SimpleKeyboardConsoleExample.java
+++ Projects/ThreadsAndGenerics/src/blocks/basic/examples/SimpleKeyboardConsoleExample.java
@@ -14,10 +14,19 @@ public class SimpleKeyboardConsoleExample
 
         Output<String> console = factory.getConsoleBlock();
         ThreadBlock<String> thread = factory.getThreadBlock();
-        PushBlock<String, String> push = factory.getPushBlock(s -> s+", for fanden!");
+        PushBlock<String, String> push = factory.getPushBlock(new FunctionExample());
         push.setOutput(console);
         thread.setInput(keyboard);
         thread.setOutput(push);
         thread.start();
+    }
+
+    private static class FunctionExample implements Function<String, String>
+    {
+        @Override
+        public String apply(String s)
+        {
+            return s+", for fanden!";
+        }
     }
 }
